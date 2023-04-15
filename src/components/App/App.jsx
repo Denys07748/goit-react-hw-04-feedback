a12d9cd060000e86eb569468fd274e2e0036b00b
Denys07748 <denis07748@gmail.com>
2023-04-15 23:55:12 +0300

    додає useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import css from './App.module.css';
 import Section from 'components/Section/Section';
 import FeedbackOptions from 'components/Feedback/FeedbackOptions';
@@ -9,14 +9,13 @@ const App = () => {
   const[good, setGood] = useState(0);
   const[neutral, setNeutral] = useState(0);
   const[bad, setBad] = useState(0);
+  const[total, setTotal] = useState(0);
+  const[percent, setPercent] = useState(0);
 
-  const countTotalFeedback = () => {
-    return good + neutral + bad;
-  };
-
-  const countPositiveFeedbackPercentage = () => {
-    return Math.round(100 * (good/countTotalFeedback()));
-  };
+  useEffect(() => {
+    setTotal(good + neutral + bad);
+    setPercent(Math.round(100 * (good/total)));
+  }, [good, neutral, bad, total]);
 
   const handleIncrementFeedback = (setOption) => {
     return setOption(state => state + 1);
@@ -31,13 +30,13 @@ const App = () => {
           />
         </Section>
         <Section title='Statistics'>
-          {countTotalFeedback() ? (
+          {total ? (
             <Statistics
               good={good}
               neutral={neutral}
               bad={bad}
-              total={countTotalFeedback()}
-              percent={countPositiveFeedbackPercentage()}
+              total={total}
+              percent={percent}
             />
           ) : (<Notification message='There is no feedback'/>)}
         </Section>
